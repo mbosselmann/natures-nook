f052c36da27c48e2c2077a656a9debd96c7a4665
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-09-08 22:43:58 +0200

    switch to css modules

--- web/src/PlantCard.tsx
+++ web/src/PlantCard.tsx
@@ -1,15 +1,15 @@
 import type { Plant } from "./App";
-import "./PlantCard.css";
+import styles from "./PlantCard.module.css";
 
 const PlantCard = ({ plant }: { plant: Plant }) => {
   return (
-    <li className="card" key={plant.name}>
-      <div className="card__header">
+    <li className={styles["card"]} key={plant.name}>
+      <div className={styles["card__header"]}>
         <h2>{plant.name}</h2>
         <p>{plant.scientific_name}</p>
       </div>
-      <p className="card__description">{plant.description}</p>
-      <ul className="card__tags">
+      <p className={styles["card__description"]}>{plant.description}</p>
+      <ul className={styles["card__tags"]}>
         {plant.tags.map((tag) => {
           return <li key={tag}>{tag}</li>;
         })}
