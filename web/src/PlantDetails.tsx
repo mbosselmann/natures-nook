4e3b0344088c64e22b6a2aedadd4109394c3dbeb
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-09-08 22:44:07 +0200

    create plant details page

---
+++ web/src/PlantDetails.tsx
@@ -0,0 +1,54 @@
+import { Plant } from "./App";
+import styles from "./PlantDetails.module.css";
+import plantUrl from "./assets/plant.svg";
+
+export default function PlantDetailsPage({ plant }: { plant: Plant }) {
+  return (
+    <main className={styles["main"]}>
+      <header className={styles["header"]}>
+        <h1>{plant.name}</h1>
+      </header>
+      <img
+        className={styles["image"]}
+        width="300"
+        height="200"
+        src={plantUrl}
+        alt={plant.name}
+      />
+      <section className={styles["section"]}>
+        <h2>{plant.scientific_name}</h2>
+        <p className={styles["description"]}>{plant.description}</p>
+        <div>
+          <h3>Light requirements:</h3>
+          <ul>
+            {plant.light_requirements.map((light, index) => (
+              <li key={index}>{light}</li>
+            ))}
+          </ul>
+        </div>
+        <h3>Water requirements:</h3>
+        <p>{plant.water_requirements}</p>
+        <h3>Care level:</h3>
+        <p>{plant.care_level}</p>
+        <div>
+          <h3>Sizes:</h3>
+          <ul>
+            {plant.sizes.map((size, index) => (
+              <li key={index}>
+                <h4>
+                  {size.height} - {size.size}
+                </h4>
+                {size.price}, Amount: {size.amount}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className={styles["tags"]}>
+          {plant.tags.map((tag) => {
+            return <li key={tag}>{tag}</li>;
+          })}
+        </ul>
+      </section>
+    </main>
+  );
+}
--- web/src/routes/plant.tsx
+++ web/src/routes/plant.tsx
@@ -1,43 +1,10 @@
 import { useLoaderData } from "react-router-dom";
 import { Plant } from "../App";
+import PlantDetails from "../PlantDetails";
 
 export default function PlantDetailsPage() {
   const { plant } = useLoaderData() as { plant: Plant };
-  return (
-    <li className="card" key={plant.name}>
-      <div className="card__header">
-        <h2>{plant.name}</h2>
-        <p>{plant.scientific_name}</p>
-      </div>
-      <p className="card__description">{plant.description}</p>
-      <section className="card__section">
-        <div>
-          <h3>Light requirements:</h3>
-          <ul>
-            {plant.light_requirements.map((light) => (
-              <li key={light}>{light}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3>Sizes:</h3>
-          <ul>
-            {plant.sizes.map((size, index) => (
-              <li key={index}>
-                <h4>
-                  {size.height} - {size.size}
-                </h4>
-                <p>{size.price}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-      <ul className="card__tags">
-        {plant.tags.map((tag) => {
-          return <li key={tag}>{tag}</li>;
-        })}
-      </ul>
-    </li>
-  );
+  console.log(plant);
+
+  return <PlantDetails plant={plant} />;
 }
