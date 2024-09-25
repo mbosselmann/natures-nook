c7e8445ea890c863a29bb610fd75ec9035ad6f7c
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-09-25 17:10:22 +0200

    fix check for data loading

--- web/src/components/PlantOverview/index.tsx
+++ web/src/components/PlantOverview/index.tsx
@@ -83,7 +83,7 @@ export default function PlantOverview() {
   }, [setFilteredPlants]);
 
   useEffect(() => {
-    if (fetchedPage.current === totalPages) {
+    if (fetchedPage.current === totalPages || fetchedPage.current === page) {
       return;
     }
 
