bc9313b17a46638885a876b0bfc801eb2c7c13c6
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-12 19:59:42 +0200

    update cart component

--- web/src/components/Cart/Item/index.tsx
+++ web/src/components/Cart/Item/index.tsx
@@ -1,9 +1,10 @@
-import { Order } from "../../../App";
-import PlantForm from "../../PlantForm";
+import { Link } from "react-router-dom";
 import { PlantSize } from "../../PlantOverview";
+import QuantityInput from "../../QuantityInput";
+import { CartItem } from "../../../App";
 
-export default function Item({ plant }: { plant: Order }) {
-  const sizes = Array.from(Object.values(plant)).filter(
+export default function Item({ item }: { item: CartItem }) {
+  const sizes = Array.from(Object.values(item)).filter(
     (size): size is PlantSize =>
       typeof size === "object" &&
       size !== null &&
@@ -11,19 +12,32 @@ export default function Item({ plant }: { plant: Order }) {
       size.amount > 0
   );
 
+  function changeAmount(size: string, action: string, value?: string) {
+    console.log(size, action, value);
+  }
+
   return (
-    <article>
-      <h3>{plant.plantName}</h3>
+    <fieldset>
+      <legend>
+        <Link to={`/plant/${item.catalogId}`}>{item.plantName}</Link>
+      </legend>
       <ul>
-        <li>
-          <PlantForm
-            type="edit"
-            order={plant}
-            sizes={sizes}
-            onSubmit={() => console.log("HELLO IT'S ME")}
-          />
-        </li>
+        {sizes.map(({ size, id, height, price, amount, available }) => (
+          <li key={id}>
+            <label>
+              {size} {price} {height} {available}
+            </label>
+            <QuantityInput
+              onDecrease={() => changeAmount(size, "decrease")}
+              onIncrease={() => changeAmount(size, "increase")}
+              size={size}
+              amount={amount}
+              available={available}
+              onChange={(value) => changeAmount(size, "change", value)}
+            />
+          </li>
+        ))}
       </ul>
-    </article>
+    </fieldset>
   );
 }
